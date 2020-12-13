1fb1f1d571a867dbb8906100693c471264c084a6
Lesther Llacuna (UG) <c0026034@newcastle.ac.uk>
2020-12-13 22:37:36 +0000

    changed algorithms list to list type

--- visualiser.py
+++ visualiser.py
@@ -7,11 +7,14 @@ from inspect import getmembers, isfunction
 import sorting
 
 
-N = 100     #number of elements
+N = 100     # number of elements
+array = [i + 1 for i in range(N)]   # array to sort
+shuffle(array)  #shuffle array
+
 
 
 algorithm_names = [o[0] for o in getmembers(sorting) if isfunction(o[1])]
-algorithms = set(getattr(sorting, algorithm_name) for algorithm_name in algorithm_names)
+algorithms = [getattr(sorting, algorithm_name) for algorithm_name in algorithm_names]
 
 
 print("Welcome to Algorithm Visualiser")
@@ -22,3 +25,6 @@ algorithm = int(input("Select which algorithm to visualise: "))
 
 print(f"You selected {algorithm_names[algorithm-1]}")
 
+algorithm = algorithms[algorithm-1]
+
+print(algorithm)
