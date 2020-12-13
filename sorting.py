9686ccbfdc7f27c6bbc3700a31e40bfe8ba1ced4
Lesther Llacuna (UG) <c0026034@newcastle.ac.uk>
2020-12-13 22:10:29 +0000

    Added bubble sort

---
+++ sorting.py
@@ -0,0 +1,10 @@
+def bubble_sort(arr):
+    length = len(arr)
+    
+    for i in range (0, length-1):
+        for j in range (0, length-i-1):
+            if arr[j] > arr[j+1]:
+                temp = arr[j+1]
+                arr[j+1] = arr[j]
+                arr[j] = temp
+                yield arr
