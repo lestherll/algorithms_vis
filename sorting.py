521e72d3e7fb5c63485fb7d8ae1c291f1833a884
Lesther Llacuna (UG) <c0026034@newcastle.ac.uk>
2020-12-14 09:03:50 +0000

    Implemented insertion sort

--- sorting.py
+++ sorting.py
@@ -7,7 +7,20 @@ def bubble_sort(arr):
                 temp = arr[j+1]
                 arr[j+1] = arr[j]
                 arr[j] = temp
-                yield arr
+            yield arr
 
 def insertion_sort(arr):
-    pass
+    length = len(arr)
+
+    for i in range(1, length):
+        curr_val = arr[i]
+        cur_pos = i
+
+        while cur_pos > 0 and arr[cur_pos-1] > curr_val:
+            arr[cur_pos] = arr[cur_pos-1]
+            cur_pos -= 1
+            
+            arr[cur_pos] = curr_val
+            yield arr
+        
+        
